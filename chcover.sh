0fe11f78171a009d1cfa5d5039623726639cb8c6
AnaLaAnsaki <oldoakstem@gmail.com>
2022-09-15 08:22:35 +0300

    added description

--- chcover.sh
+++ chcover.sh
@@ -6,5 +6,7 @@ magick "$2" .temp-"$2"-cover.pdf
 pdftk .temp-"$2"-cover.pdf .temp-"$1"-uncovered.pdf cat output "$1"
 trash-put .temp-"$2"-cover.pdf .temp-"$1"-uncovered.pdf
 
+# dependencies: pdftk, trash-cli, imagemagick
+# changes the cover of a pdf file
 # takes 2 agruments:
 # chcover [pdf file] [cover image]
