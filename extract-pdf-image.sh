4a12b243c7a661e5a0b143bb959e7d5a2541d185
AnaLaAnsaki <oldoakstem@gmail.com>
2022-09-19 09:10:49 +0300

    daily 2022-09-19 (2)

---
+++ extract-pdf-image.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+qpdf "$1" --pages "$1" "$2" -- --split-pages ."$3".pdf
+magick -density 250 ."$3"-1.pdf -quality 100 "$3".png
+#trash-put ."$3"-1.pdf
+
+
+# get-image [original file] [# of desired page] [final name w/o ext]
+#
+# $1 = original pdf name
+# $2 = page number
+# $3 = target file name (without extension)
