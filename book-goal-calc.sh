68ca1d7e548335378ff8171394e77b90ee001a22
AnaLaAnsaki <oldoakstem@gmail.com>
2022-09-22 09:57:10 +0300

    change %H to %k

--- book-goal-calc.sh
+++ book-goal-calc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 dNYE=$(date +"%j")	
-hMN=$(date +"%H")
+hMN=$(date +"%k")
 bP=$(calc "(trunc(((($dNYE*24)+$hMN)/168),2)-trunc(((($dNYE*24)+$hMN)/168),0))*100" | awk '{gsub(/^\s+|\s+$/,"")} {print $0}')
 cB=$(calc "trunc(((($dNYE*24)+$hMN)/168),0)+1" | awk '{gsub(/^\s+|\s+$/,"")} {print $0}')
