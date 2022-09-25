0636f8bf1b3e7e6c1246816900da0ff909061045
AnaLaAnsaki <oldoakstem@gmail.com>
2022-09-26 02:18:30 +0300

    daily 2022-09-26

--- terminal-art.sh
+++ terminal-art.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-echo ""
-figlet "Palestine Will Be Free" -tck -f Elite | lolcat
-figlet "Workers of the World, UNITE!" -tck -f Elite | lolcat
-echo ""
+#echo ""
+#figlet "Palestine Will Be Free" -tck -f Elite | lolcat
+#figlet "Workers of the World, UNITE!" -tck -f Elite | lolcat
+#echo ""
+fortune marxism | cowsay -p -f small -W 90 | lolcat
 echo "" 
