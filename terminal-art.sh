5bbfcdf96c383d441a2b53d075c9a137a39386bf
AnaLaAnsaki <oldoakstem@gmail.com>
2022-09-28 23:59:38 +0300

    change eyes in cowsay

--- terminal-art.sh
+++ terminal-art.sh
@@ -4,5 +4,5 @@
 #figlet "Palestine Will Be Free" -tck -f Elite | lolcat
 #figlet "Workers of the World, UNITE!" -tck -f Elite | lolcat
 #echo ""
-fortune marxism | cowsay -p -f moose -W 110 | lolcat
+fortune marxism | cowsay -e '~o' -f moose -W 110 | lolcat
 echo "" 
