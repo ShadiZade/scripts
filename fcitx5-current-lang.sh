3e57531a3cbd24a191daa1709c50029188f75eb7
AnaLaAnsaki <oldoakstem@gmail.com>
2022-10-07 15:23:16 +0300

    daily 2022-10-07

--- fcitx5-current-lang.sh
+++ fcitx5-current-lang.sh
@@ -1,20 +1,8 @@
 #!/bin/sh
 
-# Use this for FCITX5
-cim=$(qdbus "org.fcitx.Fcitx5" "/controller" "org.fcitx.Fcitx.Controller1.CurrentInputMethod")
-
-
-case $cim in 
-		keyboard-us) echo "en" 
-			;;
-		keyboard-ara) echo "ar" 
-			;;
-		keyboard-ru) echo "ru"
-			;;
-		pinyin) echo "zh"
-			;;
+case $(qdbus "org.fcitx.Fcitx5" "/controller" "org.fcitx.Fcitx.Controller1.CurrentInputMethod") in 
+		keyboard-us) echo "en" ;;
+		keyboard-ara) echo "ar" ;;
+		keyboard-ru) echo "ru" ;;
+		pinyin) echo "zh" ;;
 esac
-
-
-		
-
