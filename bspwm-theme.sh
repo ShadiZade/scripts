e8ccdb05c040736d2208bfcd4da7c630eeb5320b
Shadi Zade <oldoakstem@gmail.com>
2022-10-26 21:24:14 +0300

    tidy up theme config

--- bspwm-theme.sh
+++ bspwm-theme.sh
@@ -1,40 +1,38 @@
 #!/bin/bash
 
-bspwm_theme=frontier-blue
+bspwm_theme=industrial-black
 
-# themes
-### paprika-purple
-### frontier-blue
-### pinestone-grey
-### collective-green
-### industrial-black
-
-# colors
+# colors & themes
 bspwm_basic_color_white="ffffff"
 bspwm_basic_color_black="000000"
 bspwm_basic_color_neutral_grey="888888"
 bspwm_basic_color_dark_grey="1f2a3e"
 
+### paprika-purple
 bspwm_theme_color_paprika_purple="85132d"
 bspwm_theme_color_dignified_beige="c48e5c"
 bspwm_theme_color_dark_paprika_purple="9c1635"
 bspwm_theme_color_deep_purple="55007f"
 
+### frontier-blue
 bspwm_theme_color_frontier_blue="314664"
 bspwm_theme_color_mountainside_redwhite="b89388"
 bspwm_theme_color_dark_frontier_blue="1a2636"
 bspwm_theme_color_wispy_pink="d8787b"
 
+### pinestone-grey
 bspwm_theme_color_pinestone_grey="2f322b"
 bspwm_theme_color_pinestone_white="acab8c"
 bspwm_theme_color_mountain_ash="1d2321"
 bspwm_theme_color_overcast_blue="7e99a0"
 
+### collective-green
 bspwm_theme_color_collective_green="2c4235"
 bspwm_theme_color_monumental_gold="d7b57d"
 bspwm_theme_color_dark_collective_green="1d2c23"
 bspwm_theme_color_glorious_red="aa3b3d"
 
+### industrial-black
 bspwm_theme_color_industrial_black="201f27"
 bspwm_theme_color_scaffolding_tan="9f773f"
 bspwm_theme_color_light_industrial_black="302f3b"
@@ -82,14 +80,14 @@ esac
 
 # how to add new themes
 # 1) define colors in polybar config.ini file
-# 2) define colors in colors section of this file
-# 3) add theme name in themes section of this file with three sharps and a space
+# 2) add theme name in colors & themes section of this file with three sharps and a space
+# 3) define colors in colors & themes section of this file
 # 4) bind colors and wallpaper in definitions section of this file
 # 5) create rofi theme .rasi file with the same name as theme here
 
 # this script changes:
 # in bspwm's bspwmrc: border color and wallpaper
 # in rofi's config.rasi: theme name
-# in polybar's config.ini: color definitions
+# in polybar's config.ini: current color definitions
 #
 # in the lockscreen script: the color flags
