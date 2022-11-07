ae1cb07d957fa356943a117f960bb8a979504ed1
Shadi Zade <oldoakstem@gmail.com>
2022-11-07 22:24:57 +0300

    added 3 new themes

--- bspwm-theme.sh
+++ bspwm-theme.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-bspwm_theme=forestbeam-green
+bspwm_theme=triumphal-white
 
 # colors & themes
 bspwm_basic_color_white="ffffff"
@@ -43,6 +43,25 @@ bspwm_theme_color_forestbeam_green="82b066"
 bspwm_theme_color_autumnleaf_purple="391e27"
 bspwm_theme_color_light_forestbeam_green="bedc90"
 bspwm_theme_color_autumnleaf_yellow="bcae19"
+
+### redeye-crimson
+bspwm_theme_color_redeye_crimson="821916"
+bspwm_theme_color_festive_beige="fae19f"
+bspwm_theme_color_dark_redeye_crimson="3a0b0a"
+bspwm_theme_color_redeye_blue="54b1c0"
+
+### aero-white
+bspwm_theme_color_aero_white=$bspwm_basic_color_white
+bspwm_theme_color_aero_orange="ca5917"
+bspwm_theme_color_aero_grey="c8c8c8"
+bspwm_theme_color_aero_gold="c0a02f"
+
+### triumphal-white
+bspwm_theme_color_triumphal_white="bcbcbc"
+bspwm_theme_color_triumphal_red="961f0a"
+bspwm_theme_color_triumphal_grey="8d8d8d"
+bspwm_theme_color_triumphal_black="070707"
+
 
 # definitions
 case $bspwm_theme in
@@ -88,6 +107,28 @@ case $bspwm_theme in
 				bspwm_theme_alert_color=$bspwm_theme_color_autumnleaf_yellow
 				bspwm_theme_wallpaper="/usr/share/wallpapers/use/forestbeam.jpg"
 				;;
+			"redeye-crimson")
+				bspwm_theme_primary_color=$bspwm_theme_color_redeye_crimson
+				bspwm_theme_secondary_color=$bspwm_theme_color_festive_beige
+				bspwm_theme_dark_color=$bspwm_theme_color_dark_redeye_crimson
+				bspwm_theme_alert_color=$bspwm_theme_color_redeye_blue
+				bspwm_theme_wallpaper="/usr/share/wallpapers/use/redeyes.jpg"
+				;;
+			"aero-white")
+				bspwm_theme_primary_color=$bspwm_theme_color_aero_white
+				bspwm_theme_secondary_color=$bspwm_theme_color_aero_orange
+				bspwm_theme_dark_color=$bspwm_theme_color_aero_grey
+				bspwm_theme_alert_color=$bspwm_theme_color_aero_gold
+				bspwm_theme_wallpaper="/usr/share/wallpapers/use/aero.jpg"
+				;;
+			"triumphal-white")
+				bspwm_theme_primary_color=$bspwm_theme_color_triumphal_white
+				bspwm_theme_secondary_color=$bspwm_theme_color_triumphal_red
+				bspwm_theme_dark_color=$bspwm_theme_color_triumphal_grey
+				bspwm_theme_alert_color=$bspwm_theme_color_triumphal_black
+				bspwm_theme_wallpaper="/usr/share/wallpapers/use/triumphal-revolution.jpg"
+				;;
+
 esac
 
 
@@ -96,7 +137,7 @@ esac
 # 2) add theme name in colors & themes section of this file with three sharps and a space
 # 3) define colors in colors & themes section of this file
 # 4) bind colors and wallpaper in definitions section of this file
-# 5) create rofi theme .rasi file with the same name as theme here
+# 5) create and link rofi theme .rasi file with the same name as theme here
 
 # this script changes:
 # in bspwm's bspwmrc: border color and wallpaper
