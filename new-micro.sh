30422695e2adf6cc76116d46465e25b9d1fcf4f5
Shadi Zade <oldoakstem@gmail.com>
2022-11-17 15:40:56 +0300

    daily 2022-11-1

--- new-micro.sh
+++ new-micro.sh
@@ -1,25 +1,13 @@
 #!/bin/bash
 
- if [[ $UID != 0 ]]; then
-    echo "Please run this script with sudo."
-    exit 1
- fi
-
+[[ $UID != 0 ]] && echo "Please run this script with sudo." && exit
 case $which in
-
-		script)
-				 micro /home/oak/Repositories/scripts/"$1".sh; chmod +x-w /home/oak/Repositories/scripts/"$1".sh 
-				 ;;
-			 
-		solution) 
-				micro /home/oak/Misc/solutions/"$1".txt
-				;;
-
-		polybar-module)
-				micro /home/oak/Repositories/dotfiles/polybar/"$1".sh; chmod +x-w /home/oak/Repositories/dotfiles/polybar/"$1".sh
-				;;
-			 
-		*)
-			 	echo ":: Incorrect input"; exit 
-			 	;;
+		"script")	micro /home/oak/Repositories/scripts/"$1".sh; chmod +x-w /home/oak/Repositories/scripts/"$1".sh 
+		;;	 
+		"solution") micro /home/oak/Misc/solutions/"$1".txt
+		;;
+		"polybar-module")	micro /home/oak/Repositories/dotfiles/polybar/"$1".sh; chmod +x-w /home/oak/Repositories/dotfiles/polybar/"$1".sh
+		;;	 
+		*)	echo ":: Incorrect input"; exit 
+		;;
 esac 
--- terminal-art.sh
+++ terminal-art.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-
-#echo ""
-#figlet "Palestine Will Be Free" -tck -f Elite | lolcat
-#figlet "Workers of the World, UNITE!" -tck -f Elite | lolcat
-#echo ""
-fortune marxism | cowsay -p -f moose -W 110 | lolcat
+fortune marxism | cowsay -p -f moose -W 110 | lolcat -F 0.3
 echo "" 
