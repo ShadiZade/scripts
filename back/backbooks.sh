5f020afec23ddf03ee4d3e24952784649fe91e5e
Shadi Zade <oldoakstem@gmail.com>
2022-12-26 00:35:16 +0300

    add 2023

--- back/backbooks.sh
+++ back/backbooks.sh
@@ -8,18 +8,21 @@ echo ":: Backing booklist..."
 echo ""
 
 precount_tbr=$(cat ~/Misc/Backups/my-books/tbr.txt  | wc -l)
+precount_2023=$(cat ~/Misc/Backups/my-books/read-2023.txt  | wc -l)
 precount_2022=$(cat ~/Misc/Backups/my-books/read-2022.txt  | wc -l)
 precount_2021=$(cat ~/Misc/Backups/my-books/read-2021.txt  | wc -l)
 precount_2020=$(cat ~/Misc/Backups/my-books/read-2020.txt  | wc -l)
 precount_2019=$(cat ~/Misc/Backups/my-books/pre-2019.txt  | wc -l)
 
 find ~/Books -maxdepth 1 -type f | sort | cut -c17- > ~/Misc/Backups/my-books/tbr.txt && echo ":: TBR success!" 
+ls -1 ~/Books/read\ 2023 > ~/Misc/Backups/my-books/read-2023.txt && echo ":: Read 2023 success!" 
 ls -1 ~/Books/read\ 2022 > ~/Misc/Backups/my-books/read-2022.txt && echo ":: Read 2022 success!" 
 ls -1 ~/Books/read\ 2021 > ~/Misc/Backups/my-books/read-2021.txt && echo ":: Read 2021 success!" 
 ls -1 ~/Books/read\ 2020 > ~/Misc/Backups/my-books/read-2020.txt && echo ":: Read 2020 success!" 
 ls -1 ~/Books/pre-2019 > ~/Misc/Backups/my-books/pre-2019.txt && echo ":: Pre-2019 success!" 
 
 postcount_tbr=$(cat ~/Misc/Backups/my-books/tbr.txt  | wc -l)
+postcount_2023=$(cat ~/Misc/Backups/my-books/read-2023.txt  | wc -l)
 postcount_2022=$(cat ~/Misc/Backups/my-books/read-2022.txt  | wc -l)
 postcount_2021=$(cat ~/Misc/Backups/my-books/read-2021.txt  | wc -l)
 postcount_2020=$(cat ~/Misc/Backups/my-books/read-2020.txt  | wc -l)
@@ -28,6 +31,8 @@ postcount_2019=$(cat ~/Misc/Backups/my-books/pre-2019.txt  | wc -l)
 echo ""
 echo added $(calc "$postcount_tbr - $precount_tbr") to TBR "for a total of $postcount_tbr" \
 || echo "\033[31mWARNING: Failure to read TBR count!"
+echo added $(calc "$postcount_2023 - $precount_2023") to Read 2023 "for a total of $postcount_2023" \
+|| echo "\033[31mWARNING: Failure to read 2023 count!"
 echo added $(calc "$postcount_2022 - $precount_2022") to Read 2022 "for a total of $postcount_2022" \
 || echo "\033[31mWARNING: Failure to read 2022 count!"
 echo added $(calc "$postcount_2021 - $precount_2021") to Read 2021 "for a total of $postcount_2021" \
