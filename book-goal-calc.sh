299de76d572ce22df9820a3b4164ea1266646813
Shadi Zade <oldoakstem@gmail.com>
2023-01-08 00:19:53 +0300

    fixed trailing zeroes error

--- book-goal-calc.sh
+++ book-goal-calc.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-hours_since_newyear=$(calc "(($(date +'%j')-1)*24)+$(date +'%k')")	
+hours_since_newyear=$(calc "(($(date +'%j' | sed 's|^0*||g')-1)*24)+$(date +'%k' | sed 's|^0*||g')")	
+# sed removes trailing zeroes, which mess up the calc command.
 bP=$(calc "(trunc(($hours_since_newyear/168),2)-trunc(($hours_since_newyear/168),0))*100" | tr -d " ")
 cB=$(calc "trunc(($hours_since_newyear/168),0)+1" | tr -d " ")
+# trunc here serves as a makeshift floor function. the decimal is extracted and calculated into a percentage.
