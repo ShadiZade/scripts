2c86f90a50c87cdf6355e49d4e884f7c8a5bc5e9
Shadi Zade <oldoakstem@gmail.com>
2023-01-11 22:11:06 +0300

    update backup script

--- back/backmaster.sh
+++ back/backmaster.sh
@@ -4,10 +4,12 @@ temp_back_what=$(grep '^### ' ~/Repositories/scripts/back/backmaster.sh | tr -d 
 ### books
 ### packages
 ### wallpapers
+### music
 
 case $temp_back_what in 
 			books) ~/Repositories/scripts/back/backbooks.sh ;;
 			packages) ~/Repositories/scripts/back/backpacks.sh ;;
 			wallpapers) sudo rsync -ruv /usr/share/wallpapers/* ~/Pictures/wallpapers ;;
+			music)  ~/Repositories/scripts/back/backmusic.sh ;;
 			*) echo "Incorrect input" ;;
 esac
