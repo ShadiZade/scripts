dcb7d5ce6c11a6c858120d89795afd0a9ba9c499
Shadi Zade <oldoakstem@gmail.com>
2023-03-02 18:13:53 +0300

    add frequent-words script

---
+++ frequent-words.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+[ -z "$1" ] && read -r -p ":: Clear frequent-words? (y/N) " clr_p
+[ "$clr_p" = "y" ] && echo "" > ~/.frequent-words | sed -i '/^$/d' ~/.frequent-words && echo ":: frequent-words cleared."
+[ -z "$1" ] || echo "$1" >> ~/.frequent-words
+[ -z "$1" ] || echo ":: \"$1\" added to your frequent-words file."
+echo ":: These are the current contents of your frequent-words file:"
+echo -e "\033[31m----------------------\033[0m"
+cat ~/.frequent-words
+echo -e "\033[31m----------------------\033[0m"
+
