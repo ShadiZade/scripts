ccbe11c81965801c0200ec1937a0e5ad5a0ce633
Shadi Zade <oldoakstem@gmail.com>
2023-03-03 11:41:07 +0300

    additions to backclassical

--- back/backmaster.sh
+++ back/backmaster.sh
@@ -5,11 +5,13 @@ temp_back_what=$(grep '^### ' ~/Repositories/scripts/back/backmaster.sh | tr -d 
 ### packages
 ### wallpapers
 ### music
+### classical
 
 case $temp_back_what in 
 			books) ~/Repositories/scripts/back/backbooks.sh ;;
 			packages) ~/Repositories/scripts/back/backpacks.sh ;;
 			wallpapers) sudo rsync -ruv /usr/share/wallpapers/* ~/Pictures/wallpapers ;;
 			music)  ~/Repositories/scripts/back/backmusic.sh ;;
+			classical) ~/Repositories/scripts/back/backclassical.sh ;;
 			*) echo "Incorrect input" ;;
 esac
