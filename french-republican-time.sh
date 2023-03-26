dce110d5c0284b16e5bee6ff1ed49b74c1cd8ff2
Shadi Zade <oldoakstem@gmail.com>
2023-03-26 15:49:17 +0300

    fix formatting

--- french-republican-time.sh
+++ french-republican-time.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
+french_api_full=$(curl https://repcal.info/now.json | sed 's/,/\n/g' )
+french_formatted=$(echo $french_api_full | grep formatted | sed 2q | awk -F '"' '{print $4}')
+french_fulldate=$(echo $french_formatted | tail -n +2)
 
-french_full=$(curl https://repcal.info/now.json | sed 's/,/\n/g' | grep formatted | sed 2q | awk -F '"' '{print $4}')
-french_time=$(echo $french_full | sed 1q | awk -F ":" '{print $1":"$2}')
-french_date=$(echo $french_full | tail -n +2)
-french_month=$(echo $french_date | awk '{print $3}')
-french_day=$(echo $french_date | awk '{print $2}')
+french_time=$(echo $french_formatted | sed 1q | awk -F ":" '{print $1":"$2}')
+french_weekday=$(echo $french_fulldate | awk '{print $1}')
+french_day=$(echo $french_fulldate | awk '{print $2}')
+french_month=$(echo $french_fulldate | awk '{print $3}' | sed -e "s/\b\(.\)/\u\1/g")
+french_year=$(echo $french_api_full | grep year_arabic | awk -F ":" '{print $2}')
+
 french_french=$(grep -i "$french_month-$french_day" ~/Repositories/scripts/src/rural-cal | awk -F "|" '{print $3}')
 french_english=$(grep -i "$french_month-$french_day" ~/Repositories/scripts/src/rural-cal | awk -F "|" '{print $NF}')
-french_wikipedia=$(grep -i "$french_month-$french_day" ~/Repositories/scripts/src/rural-cal | awk -F "|" '{print $4}')
 
 telltime () {
-	notify-send -t 4000 "$french_time" "$french_date\n$french_french ($french_english)"
+	notify-send -t 6000 "$french_time" "$french_weekday\n$french_day $french_month $french_year\n$french_french ($french_english)"
 }
 
 wikithat () {
-	notify-send -t 2000 "Wikipedia" "Opening $french_english"
+	french_wikipedia=$(grep -i "$french_month-$french_day" ~/Repositories/scripts/src/rural-cal | awk -F "|" '{print $4}')
+	notify-send -t 4000 "Wikipedia" "Opening $french_english"
 	xdg-open "https://en.wikipedia.org/wiki/$french_wikipedia"
 }
--- give-in-notif.sh
+++ give-in-notif.sh
@@ -1,10 +1,10 @@
 #!/bin/zsh
 
-your_options="French Republican Calendar\nFrench Republican Honor"
+your_options="French Republican Calendar\nFrench Republican Rural Day"
 choice=$(echo -e "$your_options" | rofi -dmenu -p "Choose thingy")
 case $choice in
     "French Republican Calendar") source ~/Repositories/scripts/french-republican-time.sh; telltime ;;
-    "French Republican Honor") source ~/Repositories/scripts/french-republican-time.sh; wikithat ;;
+    "French Republican Rural Day") source ~/Repositories/scripts/french-republican-time.sh; wikithat ;;
     *) notify-send -t 1000 "Incorrect"
 esac
 
