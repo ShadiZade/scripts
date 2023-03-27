6395e241781d4fc4317a94972e8db14868b63bd0
Shadi Zade <oldoakstem@gmail.com>
2023-03-27 12:31:20 +0300

    add hijri script

--- give-in-notif.sh
+++ give-in-notif.sh
@@ -1,11 +1,12 @@
 #!/bin/zsh
 
-your_options="French Republican Calendar\nFrench Republican Rural Day\nWorld Clock"
-choice=$(echo -e "$your_options" | rofi -dmenu -p "Choose thingy")
+your_options="French Republican Calendar\nFrench Republican Rural Day\nHijri Calendar\nWorld Clock"
+choice=$(echo -e "$your_options" | rofi -i -dmenu -p "Choose thingy")
 case $choice in
     "French Republican Calendar") source ~/Repositories/scripts/french-republican-time.sh; telltime ;;
     "French Republican Rural Day") source ~/Repositories/scripts/french-republican-time.sh; wikithat ;;
     "World Clock") ~/Repositories/scripts/worldclock.sh ;;
+    "Hijri Calendar") ~/Repositories/scripts/hijri-date.sh ;;
     *) notify-send -t 1000 "Incorrect"
 esac
 
---
+++ hijri-date.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+
+hijri_raw="$(curl http://api.aladhan.com/v1/gToH/$(date +%d-%m-%Y) | sed 's/[{,]/\n/g')"
+echo -e "$hijri_raw" | grep -q "API limit exceeded" && notify-send -t 3000 "API rate limit exceeded" "try again later." && exit
+
+hijri_day="$(echo -e "$hijri_raw" | grep -m 1 day | awk -F ":" '{print $2}' | tr -d '"')"
+hijri_mon="$(echo -e "$hijri_raw" | grep -m 1 number | awk -F ":" '{print $2}')"
+hijri_yrs="$(echo -e "$hijri_raw" | grep -m 1 year | awk -F ":" '{print $2}' | tr -d '"')"
+hijri_shh="$(echo -e "$hijri_raw" | grep -m 2 en | tail -n +2 | awk -F ":" '{print $2}' | tr -d '"')"
+
+notify-send -t 6000 "$hijri_day $hijri_shh ($hijri_mon) $hijri_yrs"
