a079a1173021b44e0830cf4803f026ccfe62676d
Shadi Zade <oldoakstem@gmail.com>
2023-03-27 12:59:27 +0300

    fix strftime

--- worldclock.sh
+++ worldclock.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-la_time="$(TZ='America/Los_Angeles' date +'%H:%m on %A')"
-nyc_time="$(TZ='America/New_York' date +'%H:%m on %A')"
-utc_time="$(date -u +'%H:%m on %A')"
-my_time="$(date +'%H:%m on %A')"
-china_time="$(TZ='Asia/Shanghai' date +'%H:%m on %A')"
+
+la_time="$(TZ='America/Los_Angeles' date +'%H:%M on %A')"
+nyc_time="$(TZ='America/New_York' date +'%H:%M on %A')"
+utc_time="$(date -u +'%H:%M on %A')"
+my_time="$(date +'%H:%M on %A')"
+china_time="$(TZ='Asia/Shanghai' date +'%H:%M on %A')"
 
 notify-send -t 6000 "World Clock" "LAX: $la_time\nNYC: $nyc_time\nUTC: $utc_time\nYOU: $my_time\nCHN: $china_time"
