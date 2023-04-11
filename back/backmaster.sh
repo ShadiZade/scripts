8d0043594916c86c9430ee22fb62fb0ac4209e1b
Shadi Zade <oldoakstem@gmail.com>
2023-04-11 13:13:42 +0300

    add to backpack script

--- back/backmaster.sh
+++ back/backmaster.sh
@@ -2,14 +2,14 @@
 temp_back_what=$(grep '^### ' ~/Repositories/scripts/back/backmaster.sh | tr -d '# ' | sort -r | fzf)
 
 ### books
-### packages
+### other
 ### wallpapers
 ### music
 ### classical
 
 case $temp_back_what in 
 			books) ~/Repositories/scripts/back/backbooks.sh ;;
-			packages) ~/Repositories/scripts/back/backpacks.sh ;;
+			other) ~/Repositories/scripts/back/backpacks.sh ;;
 			wallpapers) sudo rsync -ruv /usr/share/wallpapers/* ~/Pictures/wallpapers ;;
 			music)  ~/Repositories/scripts/back/backmusic.sh ;;
 			classical) ~/Repositories/scripts/back/backclassical.sh ;;
--- back/backpacks.sh
+++ back/backpacks.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-echo ":: Backing packages..."
+back-movies-tv () {
+ls -1 ~/Movies > ~/Misc/Backups/movies-and-tv/movies.txt
+ls -1 ~/TV > ~/Misc/Backups/movies-and-tv/tv.txt
+exa --tree ~/Movies > ~/Misc/Backups/movies-and-tv/tree-movies.txt
+exa --tree ~/TV > ~/Misc/Backups/movies-and-tv/tree-tv.txt
+}
+
 pacman -Qeqn > ~/Repositories/dotfiles/arch-packages.txt && echo ":: Arch packages backed successfully!"
 pacman -Qeqm > ~/Repositories/dotfiles/aur-packages.txt && echo ":: AUR packages backed successfully!"
 fc-list > ~/Repositories/dotfiles/fonts.txt && echo ":: Fonts backed successfully!"
+back-movies-tv && echo ":: Movies and TV backed successfully!"
+
