802f09b86b66c7705a1cc6f290c23c9af690c67c
Shadi Zade <oldoakstem@gmail.com>
2023-04-14 17:57:07 +0300

    fix cases

--- mv-kebab.sh
+++ mv-kebab.sh
@@ -8,9 +8,10 @@ name_kebab="$(~/Repositories/scripts/kebabization.sh "$name_alone"; cat ~/.kebab
 name_final="$(echo $name_kebab.$ext_alone)"
 [ "$orig_name" == "$name_final" ] && echo ":: Filename is already kebab-compliant!" && exit
 [ "$orig_name" == "$name_kebab" ] && echo ":: Filename is already kebab-compliant!" && exit
+echo "$orig_name" | grep -q "\." || name_final="$name_kebab"
 echo -e ":: old name is \033[33m$orig_name\033[0m"
 echo -e ":: new name is \033[37m$name_final\033[0m"
 read -r -p ":: Perform the move? (Y/n) " proceed
 proceed=${proceed:-y}
 [ "$proceed" != "y" ] && echo ":: Move NOT performed." && exit
-echo "$orig_name" | grep -q "\." && mv -iv "$orig_name" "$name_final" || mv -iv "$orig_name" "$name_kebab"
+mv -iv "$orig_name" "$name_final"
