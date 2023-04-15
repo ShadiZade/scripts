4e15a11711ea7531986204d5f4f596bb52b94a2f
Shadi Zade <oldoakstem@gmail.com>
2023-04-15 17:13:31 +0300

    improve mpv-done

--- mpv-done.sh
+++ mpv-done.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 [ -z "$1" ] && return
-command mpv "$1"
+command mpv "$1" || exit
 fd -q done && done_exists="y" || done_exists="n"
-[ "$done_exists" = "n" ] && choose_no_done="$(echo -e 'Create done marker\nCreate done dir' | fzf --prompt='No done dir exists, what to do?')"
-[ "$choose_no_done" = "Create done marker" ] && echo "$1 is done" > ./".donefile"
+[ "$done_exists" = "n" ] && choose_no_done="$(echo -e 'Create done marker\nCreate done dir\nDo nothing' | fzf --prompt='No done dir exists, what to do? ')"
+[ "$choose_no_done" = "Do nothing" ] && echo ":: Doing nothing." && exit
+[ "$choose_no_done" = "Create done marker" ] && echo "$1 is done on $(date +"%Y-%m-%d %H:%M")." > ./".donefile" && echo ":: Done marker created."
 [ "$choose_no_done" = "Create done dir" ] && mkdir -v "done"
 fd -q done && done_exists="y" || done_exists="n"
-[ "$done_exists" = "y" ] && choose_done="$(echo -e 'yes\nno' | fzf --prompt='Move to done?')"
+[ "$done_exists" = "y" ] && choose_done="$(echo -e 'yes\nno' | fzf --prompt='Move to done? ')"
 [ "$choose_done" = "yes" ] && mv -v "$1" done/
