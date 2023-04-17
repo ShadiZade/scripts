11f5f6d7a14635389b641794efc51a5e388b3908
Shadi Zade <oldoakstem@gmail.com>
2023-04-17 22:25:49 +0300

    change kebab order to eliminate non-english letters

--- kebabization.sh
+++ kebabization.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 input_phrase="$1"
-echo -e "$input_phrase" | tr -d '"’“”!?*%$:;#@|^()[]{}<>\t' | tr -d "'" | tr '_ .,\n–—' '-' \
-| tr '[A-Z]' '[a-z]' | sed 's/--*/-/g;s/\&/and/g;s/-$//g;s/^-//g' | iconv -c -f utf8 -t ascii//TRANSLIT > ~/.kebab
+echo -e "$input_phrase" | iconv -c -f utf8 -t ascii//TRANSLIT | tr -d '"’“”!?*+=%$:;#@|^()[]{}<>\t' | tr -d "'" | tr '_ .,\n–—' '-' \
+| tr '[A-Z]' '[a-z]' | sed 's/--*/-/g;s/\&/and/g;s/-$//g;s/^-//g'  > ~/.kebab
