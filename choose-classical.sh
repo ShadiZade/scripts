06460d41a0c33611d9cafd6f80a83ad170cc710b
Shadi Zade <oldoakstem@gmail.com>
2023-04-17 22:22:59 +0300

    add necessary flags

--- choose-classical.sh
+++ choose-classical.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-music_composer=$(ls -1 ~/Misc/Backups/my-music/classical | sed 's|-works.txt||g' | rofi -dmenu -i -p "choose composer")
+music_composer=$(ls -1 ~/Misc/Backups/my-music/classical | sed 's|-works.txt||g' | rofi -normalize-match -dmenu -i -p "choose composer")
 [ -z "$music_composer" ] && exit
-quodlibet --enqueue="$(cat ~/Misc/Backups/my-music/classical/"$music_composer"-works.txt | sort -g | rofi -dmenu -i -p 'choose piece')"
+quodlibet --enqueue="$(cat ~/Misc/Backups/my-music/classical/"$music_composer"-works.txt | sort -g | rofi -normalize-match -dmenu -i -p 'choose piece')"
