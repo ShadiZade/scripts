40132da6664608deb74aad4f6ab874b682218d8a
Shadi Zade <oldoakstem@gmail.com>
2023-04-26 20:40:17 +0300

    daily 2023-04-26

--- worldclock.sh
+++ worldclock.sh
@@ -5,6 +5,9 @@ nyc_time="$(TZ='America/New_York' date +'%H:%M on %A')"
 utc_time="$(date -u +'%H:%M on %A')"
 my_time="$(date +'%H:%M on %A')"
 china_time="$(TZ='Asia/Shanghai' date +'%H:%M on %A')"
+nz_time="$(TZ='NZ' date +'%H:%M on %A')"
+
+concat="LAX: $la_time\nNYC: $nyc_time\nUTC: $utc_time\nYOU: $my_time\nCHN: $china_time\nNZL: $nz_time"
 
 pkill dunst; dunst -conf "$HOME/.config/dunst/dunstrc-left" &
-notify-send -t 6000 "World Clock" "LAX: $la_time\nNYC: $nyc_time\nUTC: $utc_time\nYOU: $my_time\nCHN: $china_time"
+notify-send -t 6000 "World Clock" "$concat"
