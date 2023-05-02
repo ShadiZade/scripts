c9b999b9313e8281e14fd1fdfc759048c8d23fd8
Shadi Zade <oldoakstem@gmail.com>
2023-05-02 12:35:06 +0300

    daily 2023-05-02

--- kebabization.sh
+++ kebabization.sh
@@ -2,4 +2,4 @@
 
 input_phrase="$1"
 echo -e "$input_phrase" | iconv -c -f utf8 -t ascii//TRANSLIT | tr -d '"’“”!?*+=%$:;#@|^~()[]{}<>\t' \
-| sed 's/\&/-and-/g' | tr -d "'" | tr '_ .,\n–—' '-' | tr '[A-Z]' '[a-z]' | sed 's/--*/-/g;s/-$//g;s/^-//g'  > ~/.kebab
+| sed 's/\&/-and-/g' | tr -d "'" | tr '_ ./,\n–—' '-' | tr '[A-Z]' '[a-z]' | sed 's/--*/-/g;s/-$//g;s/^-//g'  > ~/.kebab
--- wiki-shortcuts.sh
+++ wiki-shortcuts.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-ct="$(date '+%B %Y')"
-cat ~/Repositories/scripts/src/wiki-shortcuts | sort | sed "s/DATEHERE/$ct/g" | rofi -i -dmenu | xclip -r -selection clipboard
+cat ~/Repositories/scripts/src/wiki-shortcuts | sort | rofi -i -dmenu | xclip -r -selection clipboard
