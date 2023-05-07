2c20ef930d9708a8ff062a33bea8936516050441
Shadi Zade <oldoakstem@gmail.com>
2023-05-07 07:51:39 +0300

    fix french republican time script

--- french-republican-time.sh
+++ french-republican-time.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-french_api_full=$(curl https://repcal.info/now.json | sed 's/,/\n/g' )
+french_api_full=$(curl -L https://repcal.info/now | sed 's/,/\n/g' )
 french_formatted=$(echo $french_api_full | grep formatted | sed 2q | awk -F '"' '{print $4}')
-french_fulldate=$(echo $french_formatted | tail -n +2)
+french_fulldate=$(echo $french_api_full | grep -m 1 texts | awk -F ':' '{print $NF}' | tr -d '{}"')
 
-french_time=$(echo $french_formatted | sed 1q | awk -F ":" '{print $1":"$2}')
+french_time=$(echo $french_api_full | tail -n 3 | sed 2q | awk -F ':' '{print $NF}' | tr -d '}' | tr '\n' ':' | sed 's/:$//g')
 french_weekday=$(echo $french_fulldate | awk '{print $1}')
 french_day=$(echo $french_fulldate | awk '{print $2}')
 french_month=$(echo $french_fulldate | awk '{print $3}' | sed -e "s/\b\(.\)/\u\1/g")
-french_year=$(echo $french_api_full | grep year_arabic | awk -F ":" '{print $2}')
+french_year=$(echo $french_api_full | grep arabic | awk -F ":" '{print $NF}')
 
 french_french=$(grep -i "$french_month-$french_day|" ~/Repositories/scripts/src/rural-cal | awk -F "|" '{print $3}')
 french_english=$(grep -i "$french_month-$french_day|" ~/Repositories/scripts/src/rural-cal | awk -F "|" '{print $NF}')
