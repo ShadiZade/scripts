d5f780a9d9193449a380bc23993de0fe4f017095
Shadi Zade <oldoakstem@gmail.com>
2023-06-07 08:07:32 +0300

    daily 2023-06-07

---
+++ donefiler.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+donemovies=($(fd donefile ~/Movies | awk -F '/' '{print $5}' | sed 's/\n/ /g'))
+i=$(echo ${#donemovies})
+[ "$i" -eq 0 ] && exit
+while [ "$i" -gt 0 ]; do
+    echo -e ":: \033[33m${donemovies[$i]}\033[0m is a finished movie"
+    mv "$(ls -1 $HOME/Movies/${donemovies[$i]}/donefile*)" ~/Misc/Backups/movies-and-tv/donefiles/ && echo -e "\033[32m:: Moved donefile of ${donemovies[$i]} to backup\033[0m"
+    rm -vir "$HOME/Movies/${donemovies[$i]}"
+    i=$((i-1))
+done
+
