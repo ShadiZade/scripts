27904af459eb578e257df58ca0d48651bde89c4f
Shadi Zade <oldoakstem@gmail.com>
2023-07-16 16:23:08 +0300

    202307161623

--- kebabization.sh
+++ kebabization.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 input_phrase="$1"
-echo -e "$input_phrase" | iconv -c -f utf8 -t ascii//TRANSLIT | tr -d '"’“”!?*=$:;#@^~()[]{}<>\t' \
+echo -e "$input_phrase" | iconv -c -f utf8 -t ascii//TRANSLIT | perl -pe 's|&.*?;||g' | tr -d '"’“”!?*=$:;#@^~()[]{}<>\t' \
 | sed 's/\&/-and-/g;s/\%/-percent-/g' | tr -d "'" | tr '_ .|/+,\n–—' '-' | tr '[A-Z]' '[a-z]' | sed 's/--*/-/g;s/-$//g;s/^-//g'  > ~/.kebab
