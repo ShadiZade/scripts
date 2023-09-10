a855e444cbe8e6b1a7307e01ee96a72222445baf
Shadi Zade <oldoakstem@gmail.com>
2023-09-10 20:15:06 +0300

    202309102015

--- language-insertion.sh
+++ language-insertion.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-lang=$(echo -e "spanish\nenglish\nitalian\ngerman\nturkish" | rofi -dmenu)
+lang=$(echo -e "arabic\nspanish\nenglish\nitalian\ngerman\nturkish" | rofi -dmenu)
 [ -z "$lang" ] && exit
 rofimoji --files ~/Repositories/dotfiles/rofimoji/used-files/languages/"$lang"/*  
