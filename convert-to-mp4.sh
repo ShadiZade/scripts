2f46b750354f050ca5c4460b5644a9b673cce2d2
Shadi Zade <oldoakstem@gmail.com>
2023-09-19 07:48:02 +0300

    202309190748

---
+++ convert-to-mp4.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+working="$1"
+[ -z "$working" ] && echo -e "\033[31m:: Missing argument!\033[0m" && return
+
+function if-gif {
+    working="$(echo "$working" | sed 's/\.gif$//g')"
+    ffmpeg -i "$working".gif -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" "$working".mp4
+    mpv --loop=inf "$working".mp4
+    agreedel="$(echo -e "yes\nno" | fzf --prompt='Delete original? ')"
+    [ "$agreedel" = "yes" ] && rm -vf "$working".gif
+}
+
+function if-webm {
+    working="$(echo "$working" | sed 's/\.webm$//g')"
+    ffmpeg -i "$working".webm "$working".mp4
+    mpv --loop=inf "$working".mp4
+    agreedel="$(echo -e "yes\nno" | fzf --prompt='Delete original? ')"
+    [ "$agreedel" = "yes" ] && rm -vf "$working".webm
+}
+
+
+file --extension "$working" | grep -q "gif" && if-gif
+file --extension "$working" | grep -q "webm" && if-webm
+file --extension "$working" | grep -q "gif|webm" || exit
+
+
--- for-fun/poetryizer.sh
+++ for-fun/poetryizer.sh
@@ -6,19 +6,19 @@
 # usage: ./poetryizer [text file to be turned into objectively superior poetry to Rupi Kaur's] [level of coherence]
 #
 cp $1 ".$1-temp"
-word_count=$(cat $1 | wc -w)
+word_count="$(cat "$1" | wc -w)"
 coh="$2"
 [ -z "$coh" ] && read -r -p ":: Enter level of coherence [1-100]: " coh
 while [ "$word_count" -gt 0 ];
 do i=$((1 + $RANDOM % $coh))
-working_word=$(cat ".$1-temp" | awk -F ' ' '{print $1}')
-if [ $i == 1 ]; then
-poem=$(printf "$poem\n$working_word")
-else
-poem=$(printf "$poem $working_word")
-fi
-sed -i "s/^$working_word //g" ".$1-temp"
-word_count=$((word_count-1))
+   working_word=$(cat ".$1-temp" | awk -F ' ' '{print $1}')
+   if [ $i == 1 ]; then
+       poem=$(printf "$poem\n$working_word")
+   else
+       poem=$(printf "$poem $working_word")
+   fi
+   sed -i "s/^$working_word //g" ".$1-temp"
+   word_count=$((word_count-1))
 done
 printf "$poem" > "$1"-poem.txt
 sed -i 's/^ //g' "$1"-poem.txt
