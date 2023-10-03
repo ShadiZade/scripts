721bb15a4d5de6968737fc45cc3c66b3aa021094
Shadi Zade <oldoakstem@gmail.com>
2023-10-03 19:15:27 +0800

    202310031915

--- back/backpacks.sh
+++ back/backpacks.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
-back-movies-tv () {
-~/Repositories/scripts/donefiler.sh
-ls -1 ~/Movies > ~/Misc/Backups/video/movies.txt
-ls -1 ~/TV > ~/Misc/Backups/video/tv.txt
-exa --tree ~/Movies > ~/Misc/Backups/video/tree-movies.txt
-exa --tree ~/TV > ~/Misc/Backups/video/tree-tv.txt
+function back-movies-tv {
+    ~/Repositories/scripts/donefiler.sh
+    ls -1 ~/Movies > ~/Misc/Backups/video/movies.txt
+    ls -1 ~/TV > ~/Misc/Backups/video/tv.txt
+    exa --tree ~/Movies > ~/Misc/Backups/video/tree-movies.txt
+    exa --tree ~/TV > ~/Misc/Backups/video/tree-tv.txt
+}
+
+function back-diary {
+    cd ~/Misc/diary/
+    git add .
+    git commit -m "$(date +"%Y%m%d%H%M")"
+    cd -
 }
 
 pacman -Qeqn > ~/Repositories/dotfiles/arch-packages.txt && echo ":: Arch packages backed successfully!"
 pacman -Qeqm > ~/Repositories/dotfiles/aur-packages.txt && echo ":: AUR packages backed successfully!"
 fc-list > ~/Repositories/dotfiles/fonts.txt && echo ":: Fonts backed successfully!"
+back-diary && echo ":: Diary backed successfully!"
 back-movies-tv && echo ":: Movies and TV backed successfully!"
 
--- mpv-done.sh
+++ mpv-done.sh
@@ -3,7 +3,7 @@
 [ -z "$1" ] && return
 mpv "$1" || exit
 fd -q donefile && exit
-fd -q done && done_exists="y" || done_exists="n"
+fd -q "^done" && done_exists="y" || done_exists="n"
 [ "$done_exists" = "n" ] && choose_no_done="$(echo -e 'Create done marker\nCreate done dir\nDo nothing' | fzf --prompt='No done dir exists, what to do? ')"
 [ "$choose_no_done" = "Do nothing" ] && echo ":: Doing nothing." && exit
 [ "$choose_no_done" = "Create done marker" ] && echo "$1 is done on $(date +"%Y-%m-%d %H:%M")." > ./"donefile-$(date +"%Y-%m-%d-%H-%M")" && echo ":: Done marker created." && exit
