7b5d6d28950bf1cda9769d021510bfa1d706b39f
Shadi Zade <oldoakstem@gmail.com>
2023-12-13 11:12:49 +0800

    202312131112

--- copy-full-path.sh
+++ copy-full-path.sh
@@ -1,12 +1,6 @@
 #!/bin/bash 
 
-get-values () {
-	query_a="$1"
-	[ "$query_a" = '.' ] && path_copied="$(pwd)" || path_copied="$(echo $(pwd)/$(ls -1 | grep "$query_a"))"
-}
+[ "$1" = '.' ] && path_copied="$(pwd)" || path_copied="$(echo \'$(pwd)/$(ls -1 | grep "$1" | sed 1q | tr -d '\n')\')"
+echo "$path_copied" | tr -d '\n' | xclip -selection clipboard
+echo "$path_copied"
 
-copy-and-show () {
-	get-values $1
-	echo "$path_copied" | tr -d '\n' | xclip -selection clipboard
-	echo "$path_copied"	| tr -d '\n'
-}
