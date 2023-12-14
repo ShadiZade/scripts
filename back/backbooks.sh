b41f8f6177848463e080a1d3f698f05f1ba37494
Shadi Zade <oldoakstem@gmail.com>
2023-12-14 12:41:20 +0800

    202312141241

--- back/backbooks.sh
+++ back/backbooks.sh
@@ -7,6 +7,7 @@ bd () {
 echo ":: Backing booklist..."
 echo ""
 
+precount_2024=$(cat ~/Misc/Backups/ebook-library/read-2024.txt  | wc -l)
 precount_2023=$(cat ~/Misc/Backups/ebook-library/read-2023.txt  | wc -l)
 precount_2022=$(cat ~/Misc/Backups/ebook-library/read-2022.txt  | wc -l)
 precount_2021=$(cat ~/Misc/Backups/ebook-library/read-2021.txt  | wc -l)
@@ -15,14 +16,16 @@ precount_2019=$(cat ~/Misc/Backups/ebook-library/pre-2019.txt  | wc -l)
 precount_tbr=$(cat ~/Misc/Backups/ebook-library/tbr.txt  | wc -l)
 precount_fix=$(cat ~/Misc/Backups/ebook-library/fix.txt  | wc -l)
 
-ls -1 ~/Books/read\ 2023 > ~/Misc/Backups/ebook-library/read-2023.txt && echo ":: Read 2023 success!" 
-ls -1 ~/Books/read\ 2022 > ~/Misc/Backups/ebook-library/read-2022.txt && echo ":: Read 2022 success!" 
-ls -1 ~/Books/read\ 2021 > ~/Misc/Backups/ebook-library/read-2021.txt && echo ":: Read 2021 success!" 
-ls -1 ~/Books/read\ 2020 > ~/Misc/Backups/ebook-library/read-2020.txt && echo ":: Read 2020 success!" 
-ls -1 ~/Books/pre-2019 > ~/Misc/Backups/ebook-library/pre-2019.txt && echo ":: Pre-2019 success!" 
+ls -1 ~/Books/xdone/2024 > ~/Misc/Backups/ebook-library/read-2024.txt && echo ":: Read 2024 success!" 
+ls -1 ~/Books/xdone/2023 > ~/Misc/Backups/ebook-library/read-2023.txt && echo ":: Read 2023 success!" 
+ls -1 ~/Books/xdone/2022 > ~/Misc/Backups/ebook-library/read-2022.txt && echo ":: Read 2022 success!" 
+ls -1 ~/Books/xdone/2021 > ~/Misc/Backups/ebook-library/read-2021.txt && echo ":: Read 2021 success!" 
+ls -1 ~/Books/xdone/2020 > ~/Misc/Backups/ebook-library/read-2020.txt && echo ":: Read 2020 success!" 
+ls -1 ~/Books/xdone/2019-and-before > ~/Misc/Backups/ebook-library/pre-2019.txt && echo ":: Pre-2019 success!" 
 find ~/Books -maxdepth 1 -type f | sort | cut -c17- > ~/Misc/Backups/ebook-library/tbr.txt && echo ":: TBR success!" 
 ls -1 ~/Books/fix > ~/Misc/Backups/ebook-library/fix.txt && echo ":: Fix success!" 
 
+postcount_2024=$(cat ~/Misc/Backups/ebook-library/read-2024.txt  | wc -l)
 postcount_2023=$(cat ~/Misc/Backups/ebook-library/read-2023.txt  | wc -l)
 postcount_2022=$(cat ~/Misc/Backups/ebook-library/read-2022.txt  | wc -l)
 postcount_2021=$(cat ~/Misc/Backups/ebook-library/read-2021.txt  | wc -l)
@@ -31,6 +34,7 @@ postcount_2019=$(cat ~/Misc/Backups/ebook-library/pre-2019.txt  | wc -l)
 postcount_tbr=$(cat ~/Misc/Backups/ebook-library/tbr.txt  | wc -l)
 postcount_fix=$(cat ~/Misc/Backups/ebook-library/fix.txt  | wc -l)
 
+diff_2024=$(("$postcount_2024"-"$precount_2024"))
 diff_2023=$(("$postcount_2023"-"$precount_2023"))
 diff_2022=$(("$postcount_2022"-"$precount_2022"))
 diff_2021=$(("$postcount_2021"-"$precount_2021"))
@@ -38,9 +42,10 @@ diff_2020=$(("$postcount_2020"-"$precount_2020"))
 diff_2019=$(("$postcount_2019"-"$precount_2019"))
 diff_tbr=$(("$postcount_tbr"-"$precount_tbr"))
 diff_fix=$(("$postcount_fix"-"$precount_fix"))
-grand_total=$(("$postcount_tbr"+"$postcount_2023"+"$postcount_2022"+"$postcount_2021"+"$postcount_2020"+"$postcount_2019"+"$postcount_fix"))
+grand_total=$(("$postcount_tbr"+"$postcount_2024"+"$postcount_2023"+"$postcount_2022"+"$postcount_2021"+"$postcount_2020"+"$postcount_2019"+"$postcount_fix"))
 double_check="$(fd 'pdf|epub|mobi|djvu' ~/Books | sed '/add\//d' | wc -l)"
 
+[ $diff_2024 -eq 0 ] || diff_2024="\033[31m$diff_2024\033[0m" 
 [ $diff_2023 -eq 0 ] || diff_2023="\033[31m$diff_2023\033[0m" 
 [ $diff_2022 -eq 0 ] || diff_2022="\033[31m$diff_2022\033[0m" 
 [ $diff_2021 -eq 0 ] || diff_2021="\033[31m$diff_2021\033[0m" 
@@ -50,7 +55,9 @@ double_check="$(fd 'pdf|epub|mobi|djvu' ~/Books | sed '/add\//d' | wc -l)"
 [ $diff_fix -eq 0 ] || diff_fix="\033[31m$diff_fix\033[0m" 
 
 echo ""
-echo -e "added $diff_2023 to Read 2023 for a total of \033[4;31m$postcount_2023\033[0m" \
+echo -e "added $diff_2024 to Read 2024 for a total of \033[4;31m$postcount_2024\033[0m" \
+|| echo -e "\033[33mWARNING: Failure to read 2024 count!\033[0m"
+echo -e "added $diff_2023 to Read 2023 for a total of \033[31m$postcount_2023\033[0m" \
 || echo -e "\033[33mWARNING: Failure to read 2023 count!\033[0m"
 echo -e "added $diff_2022 to Read 2022 for a total of \033[31m$postcount_2022\033[0m" \
 || echo -e "\033[33mWARNING: Failure to read 2022 count!\033[0m"
