f8fdc1f94019db1d25ac4e237639c30ebe8cad3b
Shadi Zade <oldoakstem@gmail.com>
2023-12-18 15:29:19 +0800

    202312181529

--- OpenShennong.sh
+++ OpenShennong.sh
@@ -233,15 +233,25 @@ function count-all {
 function download-paper {
     runcase-dealer only 1
     echo -e "\033[32m:: Starting...\033[0m"
-    [ -z "$1" ] && echo -e "\033[33m:: No DOI entered.\033[0m" && exit
-    indoi="$(echo "$1" | sed 's|https://doi.org/||')" && echo -e "\033[32m:: DOI is \033[36m$indoi\033[0m"
-    shurl="$(curl -s "https://sci-hub.ru/$indoi")" && echo -e "\033[32m:: Sci-Hub queried!\033[0m"
+    [ -z "$1" ] \
+	&& echo -e "\033[33m:: No DOI entered.\033[0m" \
+	&& exit
+    indoi="$(echo "$1" | sed 's|https://doi.org/||')" \
+	&& echo -e "\033[32m:: DOI is \033[36m$indoi\033[0m"
+    shurl="$(curl -s "https://sci-hub.ru/$indoi")" \
+	&& echo -e "\033[32m:: Sci-Hub queried!\033[0m"
     ddurl="$(echo "$shurl" | grep -i 'pdf" src' | awk -F 'src="' '{print $2}' | awk -F '#' '{print $1}' | tr -d "\n")"
     ddurl="$(echo "$ddurl" | sed 's|^/downloads|sci-hub.ru/downloads|;s|^/uptodate|sci-hub.ru/uptodate|')"
     ddurl="$(echo "$ddurl" | sed 's|^/tree|sci-hub.ru/tree|;s|//||')"
-    [[ -z "$2" ]] && bibname="unnamed" || bibname="$2"
-    [[ -z "$ddurl" ]] && echo -e "\033[33m:: No file found in Sci-Hub!\033[0m"
-    [[ -z "$ddurl" ]] && exit
+    [[ -z "$2" ]] \
+	&& bibname="unnamed" \
+	    || bibname="$2"
+    fd -q "$bibname".pdf \
+	&& echo -e "\033[33m:: Paper \033[36m$bibname\033[33m already exists.\033[0m" \
+	&& exit
+    [[ -z "$ddurl" ]] \
+	&& echo -e "\033[33m:: No file found in Sci-Hub.\033[0m" \
+	&& exit
     wget -nc -O ./"$bibname".pdf -t 0 -- https://"$ddurl" && touch -c ./"$bibname".pdf
 }
 
