fd0ebe2de88b50dfab0be038f958b6092c93b883
Shadi Zade <oldoakstem@gmail.com>
2024-02-03 10:29:47 +0800

    202402031029

--- copy-full-path.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash 
-
-[ "$1" = '.' ] && path_copied="$(pwd)" || path_copied="$(echo \'$(pwd)/$(ls -1 | grep "$1" | sed 1q | tr -d '\n')\')"
-echo "$path_copied" | tr -d '\n' | xclip -selection clipboard
-echo "$path_copied"
-
