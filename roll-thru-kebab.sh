1645ec3fb1dfb0469875f4085b770e4a3a6b40ba
Shadi Zade <oldoakstem@gmail.com>
2024-02-07 23:31:51 +0800

    202402072331

--- roll-thru-kebab.sh
+++ roll-thru-kebab.sh
@@ -2,7 +2,7 @@
 source ~/Repositories/scripts/essential-functions.sh
 
 IFS=$'\n'
-file_list=($(eza -1 .))
+file_list=($(eza -1 --no-quotes .))
 for j in ${file_list[@]}
 do
     ~/Repositories/scripts/mv-kebab.sh "$j"
