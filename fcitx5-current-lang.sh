78aeb39c8670a5e90d35e4f488a86d92cc02e3da
Shadi Zade <oldoakstem@gmail.com>
2024-02-25 21:37:16 +0800

    202402252137

--- fcitx5-current-lang.sh
+++ fcitx5-current-lang.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 case $(qdbus "org.fcitx.Fcitx5" "/controller" "org.fcitx.Fcitx.Controller1.CurrentInputMethod") in 
-		keyboard-us) echo "en" ;;
-		keyboard-ara) echo "ar" ;;
-		keyboard-ru) echo "ru" ;;
-		pinyin) echo "zh" ;;
+    keyboard-us) echo "en" ;;
+    keyboard-ara) echo "ar" ;;
+    keyboard-ru) echo "ru" ;;
+    pinyin) echo "zh" ;;
 esac
