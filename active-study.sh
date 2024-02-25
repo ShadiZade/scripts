90bd6c3387cd7b8e461e98d829c6cd4e4363b696
Shadi Zade <oldoakstem@gmail.com>
2024-02-25 17:22:46 +0800

    202402251722

--- active-study.sh
+++ active-study.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
+source ~/Repositories/scripts/essential-functions.sh 
 
 storage_file="$HOME/Misc/active-study"
 
 add-study () {
 	filename_copied="$(ls -1 | grep "$1")"
-	[ -z "$filename_copied" ] && echo "nothing found." && exit
+	[ -z "$filename_copied" ] && echolor red ":: Nothing found." && exit
 	echo "$(realpath "$filename_copied")" >> $storage_file
+	echolor yellow ":: ““$filename_copied”” added to active study."
 	exit
 }
 
 remove-study () {
 	removethis="$(cat $storage_file | fzf)"
-	[ -z "$removethis" ] && echo "nothing chosen." && exit
+	[ -z "$removethis" ] && echolor red ":: Nothing chosen." && exit
 	sed -i "s|$removethis||g;/^$/d" $storage_file
-	echo $(echo $removethis | awk -F '/' '{print $NF}') was removed.
+	echolor yellow ":: ““$(echo $removethis | awk -F '/' '{print $NF}')”” was removed."
 	exit
 }
 
