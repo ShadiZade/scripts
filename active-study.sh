8a71cc687a6e4d51f2c1126a8208a6fcdbb53cfb
Shadi Zade <oldoakstem@gmail.com>
2024-02-26 10:38:21 +0800

    202402261038

--- active-study.sh
+++ active-study.sh
@@ -8,7 +8,6 @@ add-study () {
 	[ -z "$filename_copied" ] && echolor red ":: Nothing found." && exit
 	echo "$(realpath "$filename_copied")" >> $storage_file
 	echolor yellow ":: ““$filename_copied”” added to active study."
-	exit
 }
 
 remove-study () {
@@ -16,13 +15,14 @@ remove-study () {
 	[ -z "$removethis" ] && echolor red ":: Nothing chosen." && exit
 	sed -i "s|$removethis||g;/^$/d" $storage_file
 	echolor yellow ":: ““$(echo $removethis | awk -F '/' '{print $NF}')”” was removed."
-	exit
 }
 
 open-study () {
 	openthis="$(cat $storage_file | fzf)"
-	[ -z "$openthis" ] && echo "nothing chosen." && exit
-	zathura "$openthis" & exit
+	[ -z "$openthis" ] && echolor red ":: Nothing chosen." && exit
+	echolor yellow ":: Opened ““$(echo $openthis | awk -F '/' '{print $NF}')””"
+	echo -n $openthis | xclip -selection clipboard
+	zathura "$openthis"
 }
 
 view-study () {
@@ -36,11 +36,11 @@ view-study () {
 }
 
 while getopts 'vra:' OPTION; do
-	case "$OPTION" in 
-		"v") view-study ;;
-		"r") remove-study ;;
-		"a") add-study "$OPTARG" ;;
-		*) echo incorrect input; exit ;;
+    case "$OPTION" in 
+	"v") view-study ;;
+	"r") remove-study ;;
+	"a") add-study "$OPTARG" ;;
+	*) echo incorrect input; exit ;;
 	esac
 done
 (( $OPTIND == 1 )) && open-study
