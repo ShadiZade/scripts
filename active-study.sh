635962bbb659c38ce5dd0d76a126336342564734
Shadi Zade <oldoakstem@gmail.com>
2024-03-03 14:26:46 +0800

    202403031426

--- active-study.sh
+++ active-study.sh
@@ -2,30 +2,31 @@
 source ~/Repositories/scripts/essential-functions.sh 
 
 storage_file="$usdd/active-study"
+use_program="zathura"
 
-add-study () {
+function add-study {
 	filename_copied="$(ls -1 | grep "$1")"
 	[ -z "$filename_copied" ] && echolor red ":: Nothing found." && exit
 	echo "$(realpath "$filename_copied")" >> $storage_file
-	echolor yellow ":: ““$filename_copied”” added to active study."
+	echolor purple ":: ““$filename_copied”” added to active study."
 }
 
-remove-study () {
+function remove-study {
 	removethis="$(cat $storage_file | fzf)"
 	[ -z "$removethis" ] && echolor red ":: Nothing chosen." && exit
 	sed -i "s|$removethis||g;/^$/d" $storage_file
-	echolor yellow ":: ““$(echo $removethis | awk -F '/' '{print $NF}')”” was removed."
+	echolor purple ":: ““$(echo $removethis | awk -F '/' '{print $NF}')”” was removed."
 }
 
-open-study () {
+function open-study {
 	openthis="$(cat $storage_file | fzf)"
 	[ -z "$openthis" ] && echolor red ":: Nothing chosen." && exit
-	echolor yellow ":: Opened ““$(echo $openthis | awk -F '/' '{print $NF}')””"
+	echolor purple ":: Opened ““$(echo $openthis | awk -F '/' '{print $NF}')”” in ““$use_program””"
 	echo -n $openthis | xclip -selection clipboard
-	zathura "$openthis"
+	"$use_program" "$openthis" 2>/dev/null
 }
 
-view-study () {
+function view-study {
     i=1
     IFS=$'\n'
     for j in $(cat $storage_file)
@@ -35,8 +36,10 @@ view-study () {
     done
 }
 
-while getopts 'vra:' OPTION; do
-    case "$OPTION" in 
+
+while getopts 'ozvra:' OPTION; do
+    case "$OPTION" in
+	"o") use_program="okular"; open-study ;;
 	"v") view-study ;;
 	"r") remove-study ;;
 	"a") add-study "$OPTARG" ;;
