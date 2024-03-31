1a32459f2f15e66fad782cc8df156763c898756f
Shadi Zade <oldoakstem@gmail.com>
2024-04-01 02:57:19 +0800

    202404010257

--- system-check.sh
+++ system-check.sh
@@ -3,6 +3,7 @@ source ~/Repositories/scripts/essential-functions.sh
 
 logdir="$HOME/.local/logs/system-check"
 checktime=$(formatted-date-string)
+logtime="$(echo -n $checktime | cut -c-8)"
 
 function process-exists {
     echolor white-green ":: ““[CHECK]”” process ““$1”” exists."
@@ -13,9 +14,13 @@ function process-missing {
 }
 
 function process-check {
-    pidof -qx "$1" \
-	&& process-exists "$1" \
-	    || process-missing "$1"
+    pidof -qx "$1" && {
+	process-exists "$1"
+	echo "$checktime,CHECK,$1,$(pidof -x "$1")" >> "$logdir"/system-check-"$logtime".log
+    } || {
+	process-missing "$1"
+	echo "$checktime,ERROR,$1,NOPID" >> "$logdir"/system-check-"$logtime".log
+    }
 }
 
 process-check "battery-warner.sh"
