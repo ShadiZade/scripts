ed1215bcc1396c70a6e1418525aaa81d4f48d7a1
Shadi Zade <oldoakstem@gmail.com>
2024-04-08 23:59:53 +0800

    202404082359

--- give-in-notif.sh
+++ give-in-notif.sh
@@ -1,14 +1,13 @@
 #!/bin/zsh
 source ~/Repositories/scripts/essential-functions.sh
 
-your_options="French Republican Calendar\nFrench Republican Rural Day\nHijri Calendar\nSystem Info\nUpcoming Shows\nWorld Clock"
+your_options="French Republican Calendar\nFrench Republican Rural Day\nHijri Calendar\nSystem Info\nWorld Clock"
 choice=$(echo -e "$your_options" | rofi -i -dmenu -p "Choose thingy")
 case $choice in
     "French Republican Calendar") source ~/Repositories/scripts/french-republican-time.sh; telltime ;;
     "French Republican Rural Day") source ~/Repositories/scripts/french-republican-time.sh; wikithat ;;
     "Hijri Calendar") ~/Repositories/scripts/hijri-date.sh ;;
     "System Info") ~/Repositories/scripts/system-info.sh ;;
-    "Upcoming Shows") ~/Repositories/scripts/upcoming-shows.sh ;;
     "World Clock") ~/Repositories/scripts/worldclock.sh ;;
     *) killall dunst; notify-send -t 1000 "Incorrect"
 esac
--- upcoming-shows.sh
+++ upcoming-shows.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
 source ~/Repositories/scripts/essential-functions.sh
 
-clean-o () {
-	upc=$(curl "$1" | grep -E -i -A 3 -m 1 'strong>upcoming|released' | tail -n +4 | sed 's/^\s*//g')
+function clean-o {
+	upc=$(curl -s "$1" | grep -E -i -A 3 -m 1 'strong>upcoming|released' | tail -n +4 | sed 's/^\s*//g')
 	year=$(echo $upc | awk '{print $NF}')
 	day=$(echo $upc | awk '{print $2}' | tr -d ',')
 	[[ "$(echo -n $day | wc -c)" -eq 1 ]] && day="0$day"
 	month=$(echo $upc | awk '{print $1}' | cut -c-3 | tr '[a-z]' '[A-Z]')
 	upc=$(echo $day $month $year)
-	[[ -z "$upc" ]] && upc="NO UPCOMING"
+	[[ -z "$upc" ]] && upc="\033[31mNO UPCOMING\033[0m"
 	echo $upc
 }
 
-killall dunst; dunst &
-notify-send -t 3000 "Fetching Shows"
-stld="$(echo -e Lower Decks: $(clean-o https://thetvdb.com/series/star-trek-lower-decks))"
-mando="$(echo -e The Mandalorian: $(clean-o https://thetvdb.com/series/the-mandalorian))"
-severance="$(echo -e Severance: $(clean-o https://thetvdb.com/series/severance))"
-ofmd="$(echo -e Our Flag Means Death: $(clean-o https://thetvdb.com/series/our-flag-means-death))"
-ahsoka="$(echo -e Ahsoka: $(clean-o https://thetvdb.com/series/ahsoka))"
-snw="$(echo -e Strange New Worlds: $(clean-o https://thetvdb.com/series/star-trek-strange-new-worlds))"
-manhunt="$(echo -e Manhunt: $(clean-o https://thetvdb.com/series/manhunt-2022))"
+function print-out {
+    echolor blue "$1: " 1
+    echolor yellow "$(clean-o https://thetvdb.com/"$2")"
+}
 
-concat_shows="$mando\n$severance\n$snw\n$ahsoka\n$stld\n$ofmd\n$manhunt"
-
-killall dunst; dunst -conf "$HOME/.config/dunst/dunstrc-right" &
-sleep 2s
-notify-send -t 20000 "Upcoming Shows" "$concat_shows"
+print-out "Lower Decks" "/series/star-trek-lower-decks"
+print-out "The Mandalorian" "/series/the-mandalorian"
+print-out "Severance" "/series/severance"
+print-out "Our Flag Means Death" "/series/our-flag-means-death"
+print-out "Ahsoka" "/series/ahsoka"
+print-out "Strange New Worlds" "/series/star-trek-strange-new-worlds"
+print-out "Manhunt" "/series/manhunt-2022"
+print-out "Game Changer" "/series/game-changer"
