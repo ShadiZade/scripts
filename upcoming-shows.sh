6f978ec186a2e079195c9446946ef557967f1bc3
Shadi Zade <oldoakstem@gmail.com>
2024-04-23 21:37:36 +0800

    202404232137

--- upcoming-shows.sh
+++ upcoming-shows.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
 source ~/Repositories/scripts/essential-functions.sh
 
-function clean-o {
-    curlo="$(curl -s "$1")"
+function get-series {
+    echolor blue "$1: " 1
+    curlo="$(curl -s "https://thetvdb.com/series/$2")"
     upcoming="$(echo "$curlo" | grep -i -C 3 'strong>upcoming' | tail -n 1 | awk '{print $2,$1,$3}' | tr -d ',')"
     recent="$(echo "$curlo" | grep -i -C 3 recent | tail -n 1 | awk '{print $2,$1,$3}' | tr -d ',')"
     status="$(echo "$curlo" | grep -i -C 2 status | tail -n 1 | awk '{print $1}' | cut -c-4)"
-    [[ -z "$upcoming" ]] && upcoming="\033[30mno upcoming\033[0m"
-    [[ "$status" != "Cont" ]] && upcoming="\033[31mshow has ended\033[0m"
-    export recent
-    export upcoming
+    [[ -z "$upcoming" ]] && upcoming="““no upcoming””"
+    [[ -z "$recent" ]] && recent="unreleased"
+    [[ "$status" = "Ende" ]] && upcoming="\033[31mshow has ended\033[0m"
+    echolor white "$recent"
+    echolor yellow-black "\t$upcoming"
 }
 
-function print-out {
-    clean-o "https://thetvdb.com/$2"
-    echolor blue "$1: ““$recent””"
-    echolor yellow "\t$upcoming"
-}
-
-print-out "Lower Decks" "series/star-trek-lower-decks"
-print-out "Our Flag Means Death" "series/our-flag-means-death"
-print-out "Game Changer" "series/game-changer"
-print-out "The Mandalorian" "series/the-mandalorian"
-print-out "Severance" "series/severance"
-print-out "Strange New Worlds" "series/star-trek-strange-new-worlds"
+get-series "Lower Decks" "star-trek-lower-decks"
+get-series "Our Flag Means Death" "our-flag-means-death"
+get-series "Game Changer" "game-changer"
+get-series "The Green Veil" "the-green-veil"
+get-series "Manhunt" "manhunt-2022"
+get-series "The Mandalorian" "the-mandalorian"
+get-series "Severance" "severance"
+get-series "Strange New Worlds" "star-trek-strange-new-worlds"
