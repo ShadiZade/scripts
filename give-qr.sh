a67dd427f93046cc181f0d48bb7b122b5335ed05
Shadi Zade <oldoakstem@gmail.com>
2024-06-11 19:59:54 +0800

    202406111959

--- give-qr.sh
+++ give-qr.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 source ~/Repositories/scripts/essential-functions.sh
-case "$quote_p" in
-    "y") url="$(rofi -dmenu -format q -p 'Enter text')" ;;
-    *)   url="$(rofi -dmenu -p 'Enter URL')" ;;
-esac
+url="$(rofi -dmenu -p 'Enter URL')"
 [[ -z "$url" ]] && exit
 filen="$(random-string).jpg"
 qrencode -o /tmp/"$filen" "$url"
