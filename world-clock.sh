68023f4a7524f2d3d8f4c4693f6aa2254d501919
Shadi Zade <oldoakstem@gmail.com>
2024-07-04 11:54:32 +0800

    202407041154

--- world-clock.sh
+++ world-clock.sh
@@ -6,7 +6,7 @@ jo_time="$(TZ='Asia/Amman' date +'%H:%M on %A')"
 my_time="$(date +'%H:%M on %A')"
 nz_time="$(TZ='NZ' date +'%H:%M on %A')"
 
-prev_day="$(TZ='America/Los_Angeles' date +'%d')"
+prev_day="$(TZ='Pacific/Honolulu' date +'%d')"
 
 function timein {
     tzdata="$2"
@@ -16,20 +16,35 @@ function timein {
     hour="$(eval $tzdata date +'%H:%M')"
     weekday="$(eval $tzdata date +'%a')"
     day="$(eval $tzdata date +'%d')"
+    month="$(eval $tzdata date +'%m')"
     [[ "$day" -ne "$prev_day" ]] && {
-	echolor blue "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+	echolor blue "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
     }
-    echolor yellow-purple ":: ““$1”” \t— " 1
+    echolor yellow-purple ":: ““$1””\t— " 1
     echolor yellow "““$hour”” on " 1
-    echolor green "$weekday"
+    echolor yellow-green "““$weekday””, $month-$day" 1
+    [[ "$hour" = "$(date +'%H:%M')" ]] && echolor blue " <----" || echo
+	
     prev_day="$day"
 }
-echolor yellow "==============================="
+echolor yellow "======================================"
+timein "Honolulu" "Pacific/Honolulu"
 timein "Los Angeles" "America/Los_Angeles"
+timein "Denver" "America/Denver"
+timein "Chicago" "America/Chicago"
 timein "New York" "America/New_York"
+timein "São Paulo" "America/Sao_Paulo"
 timein "UTC    " "UTC"
 timein "London" "Europe/London"
-timein "Jordan" "Asia/Amman"
-timein "China" "Asia/Shanghai"
-timein "New Zealand" "NZ"
-echolor yellow "==============================="
+timein "Berlin" "Europe/Berlin"
+timein "Pretoria" "Africa/Johannesburg"
+timein "Amman" "Asia/Amman"
+timein "Tehran" "Asia/Tehran"
+timein "Kabul" "Asia/Kabul"
+timein "Hanoi" "Asia/Ho_Chi_Minh"
+timein "Beijing" "Asia/Shanghai"
+timein "Pyongyang" "Asia/Pyongyang"
+timein "Sydney" "Australia/Sydney"
+timein "Christchurch" "NZ"
+timein "Tarawa" "Pacific/Kanton"
+echolor yellow "======================================"
