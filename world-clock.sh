e2d04fd9aa0c7799a584d097ca4be3cf877f6c81
Shadi Zade <oldoakstem@gmail.com>
2024-07-04 10:48:11 +0800

    202407041048

--- world-clock.sh
+++ world-clock.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 source ~/Repositories/scripts/essential-functions.sh
 
-la_time="$(TZ='America/Los_Angeles' date +'%H:%M on %A')"
-nyc_time="$(TZ='America/New_York' date +'%H:%M on %A')"
-utc_time="$(date -u +'%H:%M on %A')"
 lon_time="$(TZ='Europe/London' date +'%H:%M on %A')"
 jo_time="$(TZ='Asia/Amman' date +'%H:%M on %A')"
 my_time="$(date +'%H:%M on %A')"
 nz_time="$(TZ='NZ' date +'%H:%M on %A')"
+
+prev_day="$(TZ='America/Los_Angeles' date +'%d')"
 
 function timein {
     tzdata="$2"
@@ -15,11 +14,22 @@ function timein {
 	tzdata="TZ=$tzdata"
     }
     hour="$(eval $tzdata date +'%H:%M')"
-    day="$(eval $tzdata date +'%A')"
+    weekday="$(eval $tzdata date +'%a')"
+    day="$(eval $tzdata date +'%d')"
+    [[ "$day" -ne "$prev_day" ]] && {
+	echolor blue "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    }
     echolor yellow-purple ":: ““$1”” \t— " 1
-    echolor yellow "““$hour”” on ““$day””"
+    echolor yellow "““$hour”” on " 1
+    echolor green "$weekday"
+    prev_day="$day"
 }
-
+echolor yellow "==============================="
 timein "Los Angeles" "America/Los_Angeles"
 timein "New York" "America/New_York"
-timein "Greenwich" "UTC"
+timein "UTC    " "UTC"
+timein "London" "Europe/London"
+timein "Jordan" "Asia/Amman"
+timein "China" "Asia/Shanghai"
+timein "New Zealand" "NZ"
+echolor yellow "==============================="
