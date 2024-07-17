8d9df15fe8f7417363a363ccb20fc45be4ddbda0
Shadi Zade <oldoakstem@gmail.com>
2024-07-17 19:03:44 +0800

    202407171903

--- mpv-done.sh
+++ mpv-done.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
 source ~/Repositories/scripts/essential-functions.sh
 
-[ -z "$1" ] \
-    && ep="$(eza -f1 --no-quotes | grep -Ev 'srt$|vtt$|part$' | fzf)" \
-	|| ep="$1"
-[ -z "$ep" ] \
-    && echo -e "\033[33m:: No file selected.\033[0m" \
-    && exit
+[[ -z "$1" ]] && {
+    ep="$(echo -e "$(eza --no-quotes -1f)\n$(fd -t l -d 1)" | sed '/^$/d' | sort | grep -Ev 'srt$|vtt$|part$' | fzf)"
+} || {
+    ep="$1"
+}
+[[ -z "$ep" ]] && {
+    echolor red ":: No file selected."
+    exit
+}
 mpv --osd-fractions --alang=eng --slang=eng "$ep" || exit
 fd -q donefile && exit
-fd -q "^done" && done_exists="y" || done_exists="n"
-[ "$done_exists" = "n" ] && choose_no_done="$(echo -e 'Create done marker\nCreate done dir\nDo nothing' | fzf --prompt='No done dir exists, what to do? ')"
-[ "$choose_no_done" = "Do nothing" ] && echo ":: Doing nothing." && exit
-[ "$choose_no_done" = "Create done marker" ] && echo "$ep is done on $(date +"%Y-%m-%d %H:%M")." > ./"donefile-$(date +"%Y-%m-%d-%H-%M")" && echo ":: Done marker created." && exit
-[ "$choose_no_done" = "Create done dir" ] && mkdir -v "done"
-fd -q done && done_exists="y" || done_exists="n"
-[ "$done_exists" = "y" ] && choose_done="$(echo -e 'yes\nno' | fzf --prompt='Move to done? ')"
+fd -q "^done$" && done_exists="y" || done_exists="n"
+[[ "$done_exists" = "n" ]] && \
+    choose_no_done="$(echo -e 'Create done marker\nCreate done dir\nDo nothing' | fzf)"
+case "$choose_no_done" in
+    "Do nothing")
+	echo ":: Doing nothing."
+	exit ;;
+    "Create done marker")
+	echo "$ep is done on $(date +"%Y-%m-%d %H:%M")." > ./"donefile-$(date +"%Y-%m-%d-%H-%M")"
+	echo ":: Done marker created."
+	exit ;;
+    "Create done dir")
+	mkdir -v "done"
+	fd -q "^done$" && done_exists="y" || done_exists="n"
+	;;
+esac
+[[ "$done_exists" = "y" ]] && choose_done="$(echo -e 'yes\nno' | fzf --prompt='Move to done? ')"
 ext_alone="$(echo "$ep" | awk -F '.' '{print $NF}')"
 allrelatedfiles="$(echo "$ep" | sed "s/\.$ext_alone//")"
-[ "$choose_done" = "yes" ] && mv -v "$allrelatedfiles"* done/
+[[ "$choose_done" = "yes" ]] && mv -v "$allrelatedfiles"* done/ 
