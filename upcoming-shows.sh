463b693fe4ef45c8b0ba7f3ecc110c06dd38c123
Shadi Zade <oldoakstem@gmail.com>
2024-07-22 17:14:31 +0800

    202407221714

--- upcoming-shows.sh
+++ upcoming-shows.sh
@@ -6,7 +6,7 @@ function get-series {
     echolor blue-white "$1 ““∎∎”” " 1
     curlo="$(curl -s "https://thetvdb.com/series/$2")"
     upcoming="$(echo "$curlo" | grep -i -C 3 'strong>upcoming' | tail -n 1 | awk '{print $2,$1,$3}' | tr -d ',')"
-    recent="$(echo "$curlo" | grep -i -C 3 recent | tail -n 1 | awk '{print $2,$1,$3}' | tr -d ',' | sed s/$'\r'//)"
+    recent="$(echo "$curlo" | grep -i -C 3 recent | tail -n 1 | awk '{print $2,$1,$3}' | tr -d ',' | tr -d $'\r')"
     status="$(echo "$curlo" | grep -i -C 2 status | tail -n 1 | awk '{print $1}' | cut -c-4)"
     [[ -z "$upcoming" ]] && upcoming="““no upcoming””"
     [[ -z "$recent" ]] && recent="““unreleased””"
@@ -14,8 +14,9 @@ function get-series {
     echolor white-black "$recent" 1
     if [[ ! "$recent" = "““unreleased””" ]]
     then
-	[[ "$(date -d "$recent" '+%Y%m%d')" = "$(date '+%Y%m%d')" ]] && echolor blue ' *' 1
-	[[ "$(date -d "$recent" '+%Y%m%d')" = "$(date -d yesterday '+%Y%m%d')" ]] && echolor red ' *' 1
+	[[ "$(date -d "$recent" '+%Y%m%d')" = "$(date '+%Y%m%d')" ]] && echolor green ' ••' 1
+	[[ "$(date -d "$recent" '+%Y%m%d')" = "$(date -d yesterday '+%Y%m%d')" ]] && echolor yellow ' ••' 1
+	[[ "$(date -d "$recent" '+%Y%m%d')" = "$(date -d '2 days ago' '+%Y%m%d')" ]] && echolor red ' ••' 1
     fi
     echolor yellow-black "\n\t$upcoming"
 }
@@ -27,3 +28,4 @@ do
     url="$(echo "$j" | xsv select 2)"
     get-series "$name" "$url"
 done
+unset IFS
