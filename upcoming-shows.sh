1cd2d2f6228956e0a94370d440c6d8bb4c464e33
Shadi Zade <oldoakstem@gmail.com>
2024-07-23 10:58:06 +0800

    202407231058

--- upcoming-shows.sh
+++ upcoming-shows.sh
@@ -5,7 +5,7 @@ shows="$usdd/shows.csv"
 function get-series {
     echolor blue-white "$1 ““∎∎”” " 1
     curlo="$(curl -s "https://thetvdb.com/series/$2")"
-    upcoming="$(echo "$curlo" | grep -i -C 3 'strong>upcoming' | tail -n 1 | awk '{print $2,$1,$3}' | tr -d ',')"
+    upcoming="$(echo "$curlo" | grep -i -C 3 'strong>upcoming' | tail -n 1 | awk '{print $2,$1,$3}' | tr -d ,$'\r')"
     recent="$(echo "$curlo" | grep -i -C 3 recent | tail -n 1 | awk '{print $2,$1,$3}' | tr -d ',' | tr -d $'\r')"
     status="$(echo "$curlo" | grep -i -C 2 status | tail -n 1 | awk '{print $1}' | cut -c-4)"
     [[ -z "$upcoming" ]] && upcoming="““no upcoming””"
@@ -14,11 +14,19 @@ function get-series {
     echolor white-black "$recent" 1
     if [[ ! "$recent" = "““unreleased””" ]]
     then
-	[[ "$(date -d "$recent" '+%Y%m%d')" = "$(date '+%Y%m%d')" ]] && echolor green ' ••' 1
-	[[ "$(date -d "$recent" '+%Y%m%d')" = "$(date -d yesterday '+%Y%m%d')" ]] && echolor yellow ' ••' 1
-	[[ "$(date -d "$recent" '+%Y%m%d')" = "$(date -d '2 days ago' '+%Y%m%d')" ]] && echolor red ' ••' 1
+	[[ "$(date -d "$recent" '+%Y%m%d')" = "$(date '+%Y%m%d')" ]] && echolor red ' •' 1
+	[[ "$(date -d "$recent" '+%Y%m%d')" = "$(date -d yesterday '+%Y%m%d')" ]] && echolor red ' ••' 1
+	[[ "$(date -d "$recent" '+%Y%m%d')" = "$(date -d '2 days ago' '+%Y%m%d')" ]] && echolor red ' •••' 1
     fi
-    echolor yellow-black "\n\t$upcoming"
+    echolor yellow-black "\n\t$upcoming" 1
+    if [[ ! "$upcoming" = "““no upcoming””" ]] && [[ ! "$upcoming" = "\033[31mshow has ended\033[0m" ]]
+    then
+	[[ "$(date -d "$upcoming" '+%Y%m%d')" = "$(date -d '3 days' '+%Y%m%d')" ]] && echolor green '\r •••' 1
+	[[ "$(date -d "$upcoming" '+%Y%m%d')" = "$(date -d '2 days' '+%Y%m%d')" ]] && echolor green '\r ••' 1
+	[[ "$(date -d "$upcoming" '+%Y%m%d')" = "$(date -d tomorrow '+%Y%m%d')" ]] && echolor green '\r •' 1
+    fi
+    echo
+	
 }
 
 IFS=$'\n'
