58704b51bfab3806b5424f2ab5c8705e07de00c7
Shadi Zade <oldoakstem@gmail.com>
2024-09-01 00:13:12 +0800

    202409010013

--- french-republican-time.sh
+++ french-republican-time.sh
@@ -7,6 +7,6 @@ command -v repcal >/dev/null || {
     exit
     # https://github.com/dekadans/repcal
 }
-datestring="$(repcal -u $(date +%z) -f '{%+A}, {%d} {%+B} ({%m}) an {%Y},')"
-ruralname="$(grep "$(repcal -u $(date +%z) -f '{%+B} {%d}')|" "$rural" | awk -F "|" '{print $3,"("$NF")"}')"
+datestring="$(repcal -f '{%+A}, {%d} {%+B} ({%m}) an {%Y},')"
+ruralname="$(grep "$(repcal -f '{%+B} {%d}')|" "$rural" | awk -F "|" '{print $3,"("$NF")"}')"
 echo "$datestring $ruralname"
