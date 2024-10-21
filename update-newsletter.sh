2974f953a88f41f430016e6ec8879e7ccb5eb2f8
ShadiZade <oldoakstem@gmail.com>
2024-10-21 18:20:00 +0800

    202410211820

--- update-newsletter.sh
+++ update-newsletter.sh
@@ -5,9 +5,11 @@ function zaytouna {
     nlname="Zaytouna"
     nl='https://www.alzaytouna.net/%D9%86%D8%B4%D8%B1%D8%A9-%D9%81%D9%84%D8%B3%D8%B7%D9%8A%D9%86-%D8%A7%D9%84%D9%8A%D9%88%D9%85/'
     loc="$HOME/Documents/newsletters/zaytouna-palestine-today"
+    dlded=''
     dlded="$(eza -1f "$loc" | grep 'pdf$' | tail -n 1 | awk -F '-' '{print $2}')"
     echolor orange ":: ❯❯ Updating ““$nlname”” now. Current edition: ““$dlded””" 1
     jinurl="$(curl -m 15 -s "$nl" | grep '\.pdf'  | awk -F '" rel="' '{print $1}' | awk -F 'href="' '{print $NF}')"
+    urled=''
     urled="$(echo "$jinurl" | awk -F '_' '{print $4}')"
     [[ -z "$urled" ]] && {
 	clear-line
@@ -45,9 +47,11 @@ function kassioun {
     nlname="Kassioun"
     nl='https://kassioun.org/pdf-archive'
     loc="$HOME/Documents/newsletters/kassioun"
+    dlded=''
     dlded="$(eza -1f "$loc" | grep 'pdf$' | tail -n 1 | awk -F '-' '{print $2}' | awk -F '.' '{print $1}')"
     echolor orange ":: ❯❯ Updating ““$nlname”” now. Current edition: ““$dlded””" 1
     jinurl="$(curl -m 15 -s "$nl" | grep 'href="/pdf-archive/' | awk -F 'href="' '{print $NF}' | tr -d '"' | sed 1q)"
+    urled=''
     urled="$(echo "$jinurl" | awk -F '-' '{print $3}' | awk -F '/' '{print $1}')"
     [[ -z "$urled" ]] && {
 	clear-line
@@ -85,9 +89,11 @@ function al-akhbar {
     nlname="Al-Akhbar"
     nl='https://al-akhbar.com/Editions'
     loc="$HOME/Documents/newsletters/al-akhbar"
+    dlded=''
     dlded="$(eza -1f "$loc" | grep 'pdf$' | tail -n 1 | awk -F '-' '{print $3}')"
     echolor orange ":: ❯❯ Updating ““$nlname”” now. Current edition: ““$dlded””" 1
     jindata="$(wget -qO- "$nl" | grep -B 5 -m 2 '<div class="row month-wrap">' | sed -n 8p)"
+    urled=''
     urled="$(echo "$jindata" | awk -F 'data-id="' '{print $NF}' | awk -F '" data-name' '{print $1}')"
     urldt="$(echo "$jindata" | awk -F 'data-name="' '{print $NF}' | awk -F '" data-old' '{print $1}')"
     [[ -z "$urled" ]] && {
