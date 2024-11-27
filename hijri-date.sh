63853fd1ef256b413c0292156971cfe204d74049
ShadiZade <oldoakstem@gmail.com>
2024-11-27 10:04:00 +0800

    202411271004

--- hijri-date.sh
+++ hijri-date.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 source ~/Repositories/scripts/essential-functions
 
-hijri_raw="$(curl -s http://api.aladhan.com/v1/gToH/$(date +%d-%m-%Y) | sed 's/[{,]/\n/g')"
-echo -e "$hijri_raw" | grep -q "API limit exceeded" && notify-send -t 3000 "API rate limit exceeded" "try again later." && exit
+jsonfile="/tmp/hijri-$(date-string ymd).json"
+curl -s http://api.aladhan.com/v1/gToH/$(date +%d-%m-%Y) > "$jsonfile"
+grep -q "API limit exceeded" "$jsonfile" && {
+    notify-send -t 3000 "Hijri API rate limit exceeded" "try again later."
+    exit 1
+}
 
-hijri_day="$(echo -e "$hijri_raw" | grep -m 1 day | awk -F ":" '{print $2}' | tr -d '"')"
-hijri_mon="$(echo -e "$hijri_raw" | grep -m 1 number | awk -F ":" '{print $2}')"
-hijri_yrs="$(echo -e "$hijri_raw" | grep -m 1 year | awk -F ":" '{print $2}' | tr -d '"')"
-hijri_shh="$(echo -e "$hijri_raw" | grep -m 2 en | tail -n +2 | awk -F ":" '{print $2}' | tr -d '"')"
-
-echo "$hijri_day $hijri_shh ($hijri_mon) $hijri_yrs"
+echo "$(jq -r '.data.hijri.day' "$jsonfile") $(jq -r '.data.hijri.month.en' "$jsonfile") ($(jq -r '.data.hijri.month.number' "$jsonfile")) $(jq -r '.data.hijri.year' "$jsonfile")"
