e9dfb4ae112e38dd4f76656685919b4263dd5bba
ShadiZade <oldoakstem@gmail.com>
2024-12-21 09:39:43 +0800

    202412210939

--- give-qr.sh
+++ give-qr.sh
@@ -3,5 +3,5 @@ source ~/Repositories/scripts/essential-functions
 url="$(rofi -dmenu -p 'Enter URL')"
 [[ -z "$url" ]] && exit
 filen="$(random-string).jpg"
-qrencode -o /tmp/"$filen" "$url"
+qrtool encode -o /tmp/"$filen" "$url"
 sxiv /tmp/"$filen"
