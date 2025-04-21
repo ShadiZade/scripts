309c93432866e2a287317e4a681fa2fbffa67d60
ShadiZade <oldoakstem@gmail.com>
2025-04-22 00:20:59 +0800

    202504220020

--- fcitx5-current-lang.sh
+++ fcitx5-current-lang.sh
@@ -4,4 +4,5 @@ case $(qdbus "org.fcitx.Fcitx5" "/controller" "org.fcitx.Fcitx.Controller1.Curre
     keyboard-ara) echo "ضـاد" | fribidi | awk '{gsub(/^\s+|\s+$/,"")} {print $0}';;
     keyboard-pt) echo "ção" ;;
     pinyin) echo "汉语" ;;
+    zhengma-pinyin) echo "郑拼" ;;
 esac
