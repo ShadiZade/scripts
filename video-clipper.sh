521e3293c407625bedaae77debc110d5108f2f0e
ShadiZade <oldoakstem@gmail.com>
2025-04-21 22:33:51 +0800

    202504212233

--- video-clipper.sh
+++ video-clipper.sh
@@ -2,32 +2,16 @@
 source ~/Repositories/scripts/essential-functions
 
 clipdir="$HOME/Videos/clips"
-
-function ask-for-timestamps {
-    echolor yellow ":: Beginning timestamp:"
-    echolor blue "- Hour: " 1
-    read -r beg_hour
-    echolor blue "- Mins: " 1
-    read -r beg_mins
-    echolor blue "- Secs: " 1
-    read -r beg_secs
-    echolor yellow ":: Ending timestamp:"
-    echolor blue "- Hour: " 1
-    read -r end_hour
-    echolor blue "- Mins: " 1
-    read -r end_mins
-    echolor blue "- Secs: " 1
-    read -r end_secs
-    export t_beg="${beg_hour}:${beg_mins}:${beg_secs}"
-    export t_end="${end_hour}:${end_mins}:${end_secs}"
-}
-
 [[ -z "$2" ]] && {
-    echolor red ":: Input error."
-    exit
+    echolor aquamarine ":: Eventual clip name: " 1
+    read -r outname
+    outname="$(kebab "$outname")"
 }
-outname="$(kebab "$2")"
-[[ -e "${outname}.mp4" ]] && {
+echolor aquamarine ":: Beginning timestamp: " 1
+read -r t_beg
+echolor aquamarine ":: Ending timestamp: " 1
+read -r t_end
+[[ -e "$outname.mp4" ]] && {
     echolor red ":: Clobber error 01!"
     exit
 }
@@ -35,5 +19,4 @@ outname="$(kebab "$2")"
     echolor red ":: Clobber error 02!"
     exit
 }
-ask-for-timestamps
 ffmpeg -i "$1" -ss "$t_beg" -to "$t_end" "$clipdir"/"$outname".mp4
