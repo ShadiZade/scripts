6bd03fb2d1124d8e381a839fe1f49c7058bc605b
ShadiZade <oldoakstem@gmail.com>
2025-06-27 16:20:53 +0800

    202506271620

--- upcoming-shows.sh
+++ upcoming-shows.sh
@@ -6,8 +6,8 @@ function get-series {
     echolor blue-white "$1 ““∎∎”” " 1
     curlo="$(curl -s "https://thetvdb.com/series/$2")"
     upcoming="$(echo "$curlo" | grep -i -C 3 'strong>upcoming' | tail -n 1 | awk '{print $2,$1,$3}' | tr -d ,$'\r')"
-    recent="$(echo "$curlo" | grep -i -C 3 recent | tail -n 1 | awk '{print $2,$1,$3}' | tr -d ',' | tr -d $'\r')"
-    status="$(echo "$curlo" | grep -i -C 2 status | tail -n 1 | awk '{print $1}' | cut -c-4)"
+    recent="$(echo "$curlo" | grep -i -C 3 'strong>recent' | tail -n 1 | awk '{print $2,$1,$3}' | tr -d ',' | tr -d $'\r')"
+    status="$(echo "$curlo" | grep -i -C 2 'strong>status' | tail -n 1 | awk '{print $1}' | cut -c-4)"
     [[ -z "$upcoming" ]] && upcoming="““no upcoming””"
     [[ -z "$recent" ]] && recent="““unreleased””"
     [[ "$status" = "Ende" ]] && upcoming="\033[31mshow has ended\033[0m"
