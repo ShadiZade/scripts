44a6be9d6aeb9d14d9249269afaf06be7c2bd8d5
ShadiZade <oldoakstem@gmail.com>
2025-12-21 23:55:23 +0800

    202512212355

--- hijri-date.sh
+++ hijri-date.sh
@@ -2,7 +2,7 @@
 source ~/Repositories/scripts/essential-functions
 
 jsonfile="/tmp/hijri-$(date-string ymd).json"
-curl -s http://api.aladhan.com/v1/gToH/$(date +%d-%m-%Y) > "$jsonfile"
+curl -s -X GET "https://api.aladhan.com/v1/gToH/$(date +%d-%m-%Y)?calendarMethod=UAQ" -H 'accept: application/json' > "$jsonfile"
 grep -q "API limit exceeded" "$jsonfile" && {
     notify-send -t 3000 "Hijri API rate limit exceeded" "try again later."
     exit 1
