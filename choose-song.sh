f2f367e00ade46202d301f52005f9fab72070260
ShadiZade <oldoakstem@gmail.com>
2026-02-20 08:45:06 +0800

    202602200845

--- choose-song.sh
+++ choose-song.sh
@@ -3,4 +3,4 @@ source ~/Repositories/scripts/essential-functions
 
 music_category=$(ls -1 ~/Misc/Backups/my-music/titles | sed 's|-titles.txt||g' | rofi -dmenu -i -p "choose category")
 [[ -z "$music_category" ]] && exit 1
-quodlibet --enqueue="$(cat ~/Misc/Backups/my-music/titles/"$music_category"-titles.txt | rofi -normalize-match -dmenu -i -p 'choose song')"
+quodlibet --enqueue="\"$(cat ~/Misc/Backups/my-music/titles/"$music_category"-titles.txt | rofi -normalize-match -dmenu -i -p 'choose song')\""
