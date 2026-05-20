288921284b0620a4f365cc980639c87887bc5840
ShadiZade <oldoakstem@gmail.com>
2026-05-20 19:53:35 +0800

    202605201953

--- back/backmaster.sh
+++ back/backmaster.sh
@@ -3,19 +3,15 @@ temp_back_what=$(grep '^### ' ~/Repositories/scripts/back/backmaster.sh | tr -d 
 
 ### books
 ### packages
-## wallpapers
 ### music
-### films
 ### classical
 ### mobile
 
 case $temp_back_what in 
     books) ~/Repositories/scripts/back/backbooks.sh ;;
     packages) ~/Repositories/scripts/back/backpacks.sh ;;
-#   wallpapers) sudo rsync -ruv /usr/share/wallpapers/* ~/Pictures/wallpapers ;;
     music)  ~/Repositories/scripts/back/backmusic.sh ;;
     classical) ~/Repositories/scripts/back/backclassical.sh ;;
-    films) ~/Repositories/scripts/back/backfilms.sh ;;
     mobile) ~/Repositories/scripts/back/backmobile.sh -a ;;
     *) echo "Incorrect input" ;;
 esac
